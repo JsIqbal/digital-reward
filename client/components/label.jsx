66e74267b4d154d5ab9f048b07e515162b8815a6
jsKraken <javascriptiqbal@gmail.com>
2023-09-03 23:24:03 +0600

    Retask KYC Form

--- client/components/label.jsx
+++ client/components/label.jsx
@@ -1,12 +1,14 @@
+import React from "react";
 import { Label } from "@/components/ui/label";
 
-const DynamicLabel = ({ htmlFor, className, content }) => {
+const DynamicLabel = ({ htmlFor, className, content, required }) => {
     return (
         <Label
             className={className ? className : ""}
             htmlFor={htmlFor ? htmlFor : ""}
         >
             {content ? content : "Default Content"}
+            {required && <span className="text-red-500 ml-1">*</span>}
         </Label>
     );
 };
